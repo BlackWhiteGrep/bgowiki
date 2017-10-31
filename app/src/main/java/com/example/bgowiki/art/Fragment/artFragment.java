8ba1a5441ac8b5745a36927cd532c34d1a116280
wangqingjin12 <531699942@qq.com>
2017-10-31 10:33:31 +0800

    实现活动页面框架（没有具体数据）

--- app/src/main/java/com/example/bgowiki/art/Fragment/artFragment.java
+++ app/src/main/java/com/example/bgowiki/art/Fragment/artFragment.java
@@ -1,30 +1,188 @@
 package com.example.bgowiki.art.Fragment;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bgowiki.R;
 import com.example.bgowiki.base.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static android.content.ContentValues.TAG;
 
 public class artFragment extends BaseFragment {
-    private TextView textView;
+    private ExpandableListView EvExpandableListView;
+    private Map<String, List<String>> dataset = new HashMap<>();
+    private String[] parentList = new String[]{"first", "second", "third"};
+    private List<String> childrenList1 = new ArrayList<>();
+    private List<String> childrenList2 = new ArrayList<>();
+    private List<String> childrenList3 = new ArrayList<>();
+    private MyExpandableListViewAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.art_expandable_layout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        EvExpandableListView = getActivity().findViewById(R.id.EvExpandableListView);
+
+        initialData();
+
+        adapter = new MyExpandableListViewAdapter();
+        EvExpandableListView.setAdapter(adapter);
+
+    }
+
+    private void initialData() {
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList2.add(parentList[1] + "-" + "first");
+        childrenList2.add(parentList[1] + "-" + "second");
+        childrenList2.add(parentList[1] + "-" + "third");
+        childrenList3.add(parentList[2] + "-" + "first");
+        childrenList3.add(parentList[2] + "-" + "second");
+        childrenList3.add(parentList[2] + "-" + "third");
+        dataset.put(parentList[0], childrenList1);
+        dataset.put(parentList[1], childrenList2);
+        dataset.put(parentList[2], childrenList3);
+    }
+
+    private class MyExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+        //  获得某个父项的某个子项
+        @Override
+        public Object getChild(int parentPos, int childPos) {
+            return dataset.get(parentList[parentPos]).get(childPos);
+        }
+
+        //  获得父项的数量
+        @Override
+        public int getGroupCount() {
+            return dataset.size();
+        }
+
+        //  获得某个父项的子项数目
+        @Override
+        public int getChildrenCount(int parentPos) {
+            return dataset.get(parentList[parentPos]).size();
+        }
+
+        //  获得某个父项
+        @Override
+        public Object getGroup(int parentPos) {
+            return dataset.get(parentList[parentPos]);
+        }
+
+        //  获得某个父项的id
+        @Override
+        public long getGroupId(int parentPos) {
+            return parentPos;
+        }
+
+        //  获得某个父项的某个子项的id
+        @Override
+        public long getChildId(int parentPos, int childPos) {
+            return childPos;
+        }
+
+        //  按函数的名字来理解应该是是否具有稳定的id，这个方法目前一直都是返回false，没有去改动过
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        //  获得父项显示的view
+        @Override
+        public View getGroupView(int parentPos, boolean b, View view, ViewGroup viewGroup) {
+
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.art_parent_item, null);
+            }
+            view.setTag(R.layout.art_parent_item, parentPos);
+            view.setTag(R.layout.art_child_item, -1);
+            TextView text =  view.findViewById(R.id.parent_title);
+            text.setText(parentList[parentPos]);
+            return view;
+        }
+
+        //  获得子项显示的view
+        @Override
+        public View getChildView(int parentPos, int childPos, boolean b, View view, ViewGroup viewGroup) {
+            if (view == null) {
+
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.art_child_item, null);
+            }
+            view.setTag(R.layout.art_parent_item, parentPos);
+            view.setTag(R.layout.art_child_item, childPos);
+            TextView text =  view.findViewById(R.id.child_title);
+            text.setText(dataset.get(parentList[parentPos]).get(childPos));
+            text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "点到了内置的textview", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return view;
+        }
+
+        //  子项是否可选中，如果需要设置子项的点击事件，需要返回true
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return false;
+        }
+
+
+    }
+
+
+
+
 
     @Override
     public View initView() {
-        Log.e(TAG,"被初始化了");
-        textView = new TextView(mcontext);
-        textView.setText("主页面");
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        return textView;
+        View view = View.inflate(mcontext, R.layout.sevent, null);
+        return view;
+
     }
 
     @Override
     public void initDate() {
         super.initDate();
         Log.e(TAG,"数据被初始化了");
-        textView.setText("主页面内容");
     }
 }
