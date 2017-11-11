6f3a557acb928bb69849d19f39fd360cedcd4dc5
BlackWhiteGrep <1023042207@qq.com>
2017-11-11 17:03:31 +0800

    完成了对于Spinner的监听和传值。 已知BUG：不能够同时填写多个大数目标

--- app/src/main/java/com/example/bgowiki/activity/wherematerialActivity.java
+++ app/src/main/java/com/example/bgowiki/activity/wherematerialActivity.java
@@ -2,13 +2,17 @@ package com.example.bgowiki.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import com.example.bgowiki.R;
+import com.example.bgowiki.home.Fragment.SeventFragment;
 
 public class wherematerialActivity extends AppCompatActivity {
+    private SeventFragment fragment;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FragmentManager fragmentManager = getSupportFragmentManager();
         setContentView(R.layout.material);
     }
 }
